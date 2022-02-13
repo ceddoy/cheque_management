aa9ed47dd1edacba28a6fd8c93c2987ed22bf6b5
andrew <ceddoy@mail.ru>
2022-02-13 17:59:10 +0700

    добавил кэширование для списка чеков

--- chequeapp/urls.py
+++ chequeapp/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, include
+from django.views.decorators.cache import cache_page
 
 from chequeapp import views as chequeapp
 
@@ -6,6 +7,6 @@ app_name = 'chequeapp'
 
 urlpatterns = [
     path('create_checks/', chequeapp.CreateCheckAPIView.as_view()),
-    path('new_checks/', chequeapp.NewChecksListAPIView.as_view()),
+    path('new_checks/', cache_page(60 * 15)(chequeapp.NewChecksListAPIView.as_view())),
     path('check/', chequeapp.CheckAPIView.as_view()),
 ]
